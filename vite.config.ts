d181488ae4be4f2fe1eedc7c7a21afe8d0816085
ymzk827 <emuzyka827@gmail.com>
2026-01-21 02:47:26 +0100

    1

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/page/',
+  base: '/FT_zavere-nyproject/',
   plugins: [
     vue(),
     vueJsx(),
